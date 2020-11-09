bd552f023973ed0b32e4efc73799f09422f709b2
AwesomeCodingGuy <sonla@gmx.net>
2020-11-09 18:12:48 +0100

    Added string constants

---
+++ src/data/constants.h
@@ -0,0 +1,29 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <QString>
+
+namespace constants {
+
+// Github jgehrcke
+extern const QString germanyGithubRepoUrl;
+extern const QString germanyRepoDataUrl;
+
+extern const QString casesByState;
+extern const QString casesByAgs;
+extern const QString deathsByState;
+extern const QString deathsByAgs;
+extern const QString ags;
+
+// Github JHU
+extern const QString jhuGithubRepoUrl;
+extern const QString jhuRepoDataUrl;
+
+extern const QString casesUS;
+extern const QString casesGlobal;
+extern const QString deathsUS;
+extern const QString deathsGlobal;
+
+}
+
+#endif // CONSTANTS_H
