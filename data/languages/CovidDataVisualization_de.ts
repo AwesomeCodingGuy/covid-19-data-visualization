d5bcc8fa46a25afa2c6c5a328f2644752acaa892
AwesomeCodingGuy <sonla@gmx.net>
2020-12-12 23:26:52 +0100

    Added language files with translations for german and english.

---
+++ data/languages/CovidDataVisualization_de.ts
@@ -0,0 +1,271 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>ChartWidget</name>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="70"/>
+        <source>Erkrankungen kumuliert</source>
+        <translation>Erkrankungen kumuliert</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="71"/>
+        <source>Todesfälle kumuliert</source>
+        <translation>Todesfälle kumuliert</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="72"/>
+        <source>Tägliche Neuerkrankungen</source>
+        <translation>Tägliche Neuerkrankungen</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="73"/>
+        <source>Tägliche Todesfälle</source>
+        <translation>Tägliche Todesfälle</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="74"/>
+        <location filename="../../src/widgets/chartwidget.cpp" line="264"/>
+        <location filename="../../src/widgets/chartwidget.cpp" line="318"/>
+        <source>7-Tage-Inzidenz</source>
+        <translation>7-Tage-Inzidenz</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="200"/>
+        <source>Zoom zurücksetzen</source>
+        <translation>Zoom zurücksetzen</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="217"/>
+        <location filename="../../src/widgets/chartwidget.cpp" line="258"/>
+        <source>Erkrankungen</source>
+        <translation>Erkrankungen</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="220"/>
+        <location filename="../../src/widgets/chartwidget.cpp" line="261"/>
+        <location filename="../../src/widgets/chartwidget.cpp" line="315"/>
+        <source>Todesfälle</source>
+        <translation>Todesfälle</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="237"/>
+        <source>Fälle kumuliert</source>
+        <translation>Fälle kumuliert</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="287"/>
+        <source>Fälle / Tag</source>
+        <translation>Fälle / Tag</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="312"/>
+        <source>Fälle</source>
+        <translation>Fälle</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="363"/>
+        <source>Beschleunigung / Tag</source>
+        <translation>Beschleunigung / Tag</translation>
+    </message>
+</context>
+<context>
+    <name>ColorLegend</name>
+    <message>
+        <location filename="../../src/widgets/colorlegend.cpp" line="15"/>
+        <source>7-Tage-Inzidenz</source>
+        <translation>7-Tage-Inzidenz</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/colorlegend.cpp" line="29"/>
+        <location filename="../../src/widgets/colorlegend.cpp" line="35"/>
+        <location filename="../../src/widgets/colorlegend.cpp" line="39"/>
+        <source>Wert</source>
+        <translation>Wert</translation>
+    </message>
+</context>
+<context>
+    <name>CovidDataTreeModel</name>
+    <message>
+        <location filename="../../src/models/coviddatatreemodel.cpp" line="9"/>
+        <source>Datensatz</source>
+        <translation>Datensatz</translation>
+    </message>
+</context>
+<context>
+    <name>MainContentWidget</name>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="81"/>
+        <source>Karten</source>
+        <translation>Karten</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="83"/>
+        <source>Diagramme</source>
+        <translation>Diagramme</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="85"/>
+        <source>Daten herunterladen / aktualisieren</source>
+        <translation>Daten herunterladen / aktualisieren</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="136"/>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="275"/>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="303"/>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="309"/>
+        <source>Vergleichen</source>
+        <translation>Vergleichen</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="139"/>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="267"/>
+        <source>Auswahl aufheben</source>
+        <translation>Auswahl aufheben</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="148"/>
+        <source>Einstellungen</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="187"/>
+        <source>Dateien werden heruntergeladen...</source>
+        <translation>Dateien werden heruntergeladen...</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="237"/>
+        <source>Fehler</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="237"/>
+        <source>Keine Daten vorhanden</source>
+        <translation>Keine Daten vorhanden</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="263"/>
+        <source>TreeView</source>
+        <translation>TreeView</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="304"/>
+        <source>Es sind zu viele Daten ausgewählt.
+Bitte beschränken Sie sich auf 17 Datensätze.</source>
+        <translation>Es sind zu viele Daten ausgewählt.
+Bitte beschränken Sie sich auf 17 Datensätze.</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="310"/>
+        <source>Zum Vergleichen müssen mindestens 2 Datensätze ausgewählt sein.</source>
+        <translation>Zum Vergleichen müssen mindestens 2 Datensätze ausgewählt sein.</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="323"/>
+        <source>Vergleich</source>
+        <translation>Vergleich</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="358"/>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="365"/>
+        <source>Information</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="359"/>
+        <source>Es sind keine Daten vorhanden.
+
+Bitte drücken sie den Button zum Herunterladen der aktuellen Daten.</source>
+        <translation>Es sind keine Daten vorhanden.
+
+Bitte drücken sie den Button zum Herunterladen der aktuellen Daten.</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="366"/>
+        <source>Es fehlen Daten. Die Anwendung kann mit den vorhandenen Daten fortfahren.
+
+Möchten Sie den Ladevorgang fortsetzen?</source>
+        <translation>Es fehlen Daten. Die Anwendung kann mit den vorhandenen Daten fortfahren.
+
+Möchten Sie den Ladevorgang fortsetzen?</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="386"/>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="394"/>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="402"/>
+        <source>Datensätze werden gelesen...
+</source>
+        <translation>Datensätze werden gelesen...
+</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="386"/>
+        <source>Deutschland</source>
+        <translation>Deutschland</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="394"/>
+        <source>USA</source>
+        <translation>USA</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="402"/>
+        <source>World</source>
+        <translation>Welt</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="410"/>
+        <source>Karten werden gemalt...</source>
+        <translation>Karten werden gemalt...</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../src/widgets/mainwindow.cpp" line="92"/>
+        <source>Anwendung schließen</source>
+        <translation>Anwendung schließen</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/mainwindow.cpp" line="93"/>
+        <source>Möchten Sie die Anwendung wirklich schließen?</source>
+        <translation>Möchten Sie die Anwendung wirklich schließen?</translation>
+    </message>
+</context>
+<context>
+    <name>MapWidget</name>
+    <message>
+        <location filename="../../src/widgets/mapwidget.cpp" line="59"/>
+        <source>Farblegende</source>
+        <translation>Farblegende</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../src/utils/areaitem.cpp" line="145"/>
+        <source>Heute gemeldete Fälle</source>
+        <translation>Heute gemeldete Fälle</translation>
+    </message>
+    <message>
+        <location filename="../../src/utils/areaitem.cpp" line="148"/>
+        <source>Heute gemeldete Todesfälle</source>
+        <translation>Heute gemeldete Todesfälle</translation>
+    </message>
+    <message>
+        <location filename="../../src/utils/areaitem.cpp" line="151"/>
+        <source>Fälle insgesamt</source>
+        <translation>Fälle insgesamt</translation>
+    </message>
+    <message>
+        <location filename="../../src/utils/areaitem.cpp" line="154"/>
+        <source>Todesfälle insgesamt</source>
+        <translation>Todesfälle insgesamt</translation>
+    </message>
+    <message>
+        <location filename="../../src/utils/areaitem.cpp" line="157"/>
+        <source>7-Tage-Inzidenz</source>
+        <translation>7-Tage-Inzidenz</translation>
+    </message>
+</context>
+</TS>
---
+++ data/languages/CovidDataVisualization_en.ts
@@ -0,0 +1,271 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>ChartWidget</name>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="70"/>
+        <source>Erkrankungen kumuliert</source>
+        <translation>Cumulated cases</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="71"/>
+        <source>Todesfälle kumuliert</source>
+        <translation>Cumulated deaths</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="72"/>
+        <source>Tägliche Neuerkrankungen</source>
+        <translation>Daily new cases</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="73"/>
+        <source>Tägliche Todesfälle</source>
+        <translation>Daily deaths</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="74"/>
+        <location filename="../../src/widgets/chartwidget.cpp" line="264"/>
+        <location filename="../../src/widgets/chartwidget.cpp" line="318"/>
+        <source>7-Tage-Inzidenz</source>
+        <translation>7-Day-Incidence</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="200"/>
+        <source>Zoom zurücksetzen</source>
+        <translation>Reset zoom</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="217"/>
+        <location filename="../../src/widgets/chartwidget.cpp" line="258"/>
+        <source>Erkrankungen</source>
+        <translation>Cases</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="220"/>
+        <location filename="../../src/widgets/chartwidget.cpp" line="261"/>
+        <location filename="../../src/widgets/chartwidget.cpp" line="315"/>
+        <source>Todesfälle</source>
+        <translation>Deaths</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="237"/>
+        <source>Fälle kumuliert</source>
+        <translation>Cumulated cases</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="287"/>
+        <source>Fälle / Tag</source>
+        <translation>Cases / Day</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="312"/>
+        <source>Fälle</source>
+        <translation>Cases</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/chartwidget.cpp" line="363"/>
+        <source>Beschleunigung / Tag</source>
+        <translation>Acceleration / Day</translation>
+    </message>
+</context>
+<context>
+    <name>ColorLegend</name>
+    <message>
+        <location filename="../../src/widgets/colorlegend.cpp" line="15"/>
+        <source>7-Tage-Inzidenz</source>
+        <translation>7-Day-Incidence</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/colorlegend.cpp" line="29"/>
+        <location filename="../../src/widgets/colorlegend.cpp" line="35"/>
+        <location filename="../../src/widgets/colorlegend.cpp" line="39"/>
+        <source>Wert</source>
+        <translation>Value</translation>
+    </message>
+</context>
+<context>
+    <name>CovidDataTreeModel</name>
+    <message>
+        <location filename="../../src/models/coviddatatreemodel.cpp" line="9"/>
+        <source>Datensatz</source>
+        <translation>Dataset</translation>
+    </message>
+</context>
+<context>
+    <name>MainContentWidget</name>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="81"/>
+        <source>Karten</source>
+        <translation>Maps</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="83"/>
+        <source>Diagramme</source>
+        <translation>Charts</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="85"/>
+        <source>Daten herunterladen / aktualisieren</source>
+        <translation>Download / update data</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="136"/>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="275"/>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="303"/>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="309"/>
+        <source>Vergleichen</source>
+        <translation>Compare</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="139"/>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="267"/>
+        <source>Auswahl aufheben</source>
+        <translation>Clear selection</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="148"/>
+        <source>Einstellungen</source>
+        <translation>Options</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="187"/>
+        <source>Dateien werden heruntergeladen...</source>
+        <translation>Downloading files...</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="237"/>
+        <source>Fehler</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="237"/>
+        <source>Keine Daten vorhanden</source>
+        <translation>No data available</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="263"/>
+        <source>TreeView</source>
+        <translation>TreeView</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="304"/>
+        <source>Es sind zu viele Daten ausgewählt.
+Bitte beschränken Sie sich auf 17 Datensätze.</source>
+        <translation>Too many selected items.
+Please limit the selection to 17 datasets.</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="310"/>
+        <source>Zum Vergleichen müssen mindestens 2 Datensätze ausgewählt sein.</source>
+        <translation>Select a minimum of 2 datasets to compare.</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="323"/>
+        <source>Vergleich</source>
+        <translation>Comparison</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="358"/>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="365"/>
+        <source>Information</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="359"/>
+        <source>Es sind keine Daten vorhanden.
+
+Bitte drücken sie den Button zum Herunterladen der aktuellen Daten.</source>
+        <translation>No data available.
+
+Please press the button to download current data files.</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="366"/>
+        <source>Es fehlen Daten. Die Anwendung kann mit den vorhandenen Daten fortfahren.
+
+Möchten Sie den Ladevorgang fortsetzen?</source>
+        <translation>Missing data. The application can continue with available data.
+
+Do you want to continue?</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="386"/>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="394"/>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="402"/>
+        <source>Datensätze werden gelesen...
+</source>
+        <translation>Reading datasets...
+</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="386"/>
+        <source>Deutschland</source>
+        <translation>Germany</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="394"/>
+        <source>USA</source>
+        <translation>USA</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="402"/>
+        <source>World</source>
+        <translation>World</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/maincontentwidget.cpp" line="410"/>
+        <source>Karten werden gemalt...</source>
+        <translation>Drawing maps...</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../src/widgets/mainwindow.cpp" line="92"/>
+        <source>Anwendung schließen</source>
+        <translation>Close application</translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/mainwindow.cpp" line="93"/>
+        <source>Möchten Sie die Anwendung wirklich schließen?</source>
+        <translation>Are you sure to quit the application?</translation>
+    </message>
+</context>
+<context>
+    <name>MapWidget</name>
+    <message>
+        <location filename="../../src/widgets/mapwidget.cpp" line="59"/>
+        <source>Farblegende</source>
+        <translation>Color legend</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../src/utils/areaitem.cpp" line="145"/>
+        <source>Heute gemeldete Fälle</source>
+        <translation>Reported cases today</translation>
+    </message>
+    <message>
+        <location filename="../../src/utils/areaitem.cpp" line="148"/>
+        <source>Heute gemeldete Todesfälle</source>
+        <translation>Reported deaths today</translation>
+    </message>
+    <message>
+        <location filename="../../src/utils/areaitem.cpp" line="151"/>
+        <source>Fälle insgesamt</source>
+        <translation>Total cases</translation>
+    </message>
+    <message>
+        <location filename="../../src/utils/areaitem.cpp" line="154"/>
+        <source>Todesfälle insgesamt</source>
+        <translation>Total deaths</translation>
+    </message>
+    <message>
+        <location filename="../../src/utils/areaitem.cpp" line="157"/>
+        <source>7-Tage-Inzidenz</source>
+        <translation>7-Day-Incidence</translation>
+    </message>
+</context>
+</TS>
